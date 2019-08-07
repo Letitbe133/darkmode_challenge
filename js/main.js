77869cf0b43e2150af865cfc3288b8f972b19395
letitbe133 <lionel.lft.com>
2019-08-07 15:45:25 +0200

    dark mode example using theme object

--- js/main.js
+++ js/main.js
@@ -1,30 +1,38 @@
+// theme object
+const theme = {
+  dark: {
+    background: "#333",
+    emphasis: "#1c1b1b",
+    text: "#ccc",
+    shadow: "5px 5px 20px #1c1b1b",
+    dataAttribute: "light",
+    switcherText: "Light Theme"
+  },
+  light: {
+    background: "#fff",
+    emphasis: "rgb(250, 169, 103)",
+    text: "#333",
+    shadow: "5px 5px 20px #ccc",
+    dataAttribute: "dark",
+    switcherText: "Dark Theme"
+  }
+};
+
 // DOM elements
 const switcher = document.querySelector("#mode-switcher");
 const body = document.querySelector("body");
 const main = document.querySelector("main");
 
-let darkModeEnabled = false;
-
 // functions
-
 const switchMode = () => {
-  if (darkModeEnabled) {
-    body.style.background = "#fff";
-    switcher.style.background = "#ccc";
-    main.style.background = "rgb(250, 169, 103)";
-    main.style.boxShadow = "5px 5px 20px #ccc";
-    switcher.style.color = "#333";
-    switcher.innerHTML = "Dark Mode";
-    darkModeEnabled = false;
-  } else {
-    body.style.background = "#333";
-    switcher.style.background = "#1c1b1b";
-    main.style.background = "#1c1b1b";
-    main.style.boxShadow = "5px 5px 20px #1c1b1b";
-    switcher.style.color = "#ccc";
-    switcher.innerHTML = "Light Mode";
-    darkModeEnabled = true;
-  }
+  let mode = switcher.dataset.mode;
+  body.style.background = theme[mode].background;
+  switcher.style.background = theme[mode].emphasis;
+  main.style.background = theme[mode].emphasis;
+  main.style.boxShadow = theme[mode].shadow;
+  switcher.style.color = theme[mode].text;
+  switcher.innerHTML = theme[mode].switcherText;
+  switcher.dataset.mode = theme[mode].dataAttribute;
 };
 
 switcher.addEventListener("click", switchMode);
