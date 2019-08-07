72b734e474795f32593c3cd7ffa709ffa1221962
letitbe133 <lionel.lft.com>
2019-08-07 12:30:41 +0200

    dark mode example using classes

--- js/main.js
+++ js/main.js
@@ -3,28 +3,19 @@ const switcher = document.querySelector("#mode-switcher");
 const body = document.querySelector("body");
 const main = document.querySelector("main");
 
-let darkModeEnabled = false;
-
 // functions
 
+const toggleClass = (element, className) => {
+  element.classList.toggle(className);
+};
+
 const switchMode = () => {
-  if (darkModeEnabled) {
-    body.style.background = "#fff";
-    switcher.style.background = "#ccc";
-    main.style.background = "rgb(250, 169, 103)";
-    main.style.boxShadow = "5px 5px 20px #ccc";
-    switcher.style.color = "#333";
-    switcher.innerHTML = "Dark Mode";
-    darkModeEnabled = false;
-  } else {
-    body.style.background = "#333";
-    switcher.style.background = "#1c1b1b";
-    main.style.background = "#1c1b1b";
-    main.style.boxShadow = "5px 5px 20px #1c1b1b";
-    switcher.style.color = "#ccc";
-    switcher.innerHTML = "Light Mode";
-    darkModeEnabled = true;
-  }
+  toggleClass(body, "dark-bg");
+  toggleClass(main, "dark-bg--darken");
+  toggleClass(switcher, "dark-bg--darken");
+  toggleClass(main, "dark-shadow");
+  toggleClass(switcher, "dark-text");
+  switcher.innerHTML = "Light Mode";
 };
 
 switcher.addEventListener("click", switchMode);
